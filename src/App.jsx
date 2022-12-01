360bf4f0ed9414d8f63d947c238bd8ef4bd331ee
Oleksii Kravchuk <44169697+shkiperoO@users.noreply.github.com>
2022-12-01 00:55:04 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {StockOverviewPage} from './pages/StockOverviewPage.jsx'
+import {StockDetailPage} from './pages/StockDetailPage.jsx'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StockOverviewPage />}/>
+          <Route path="/detail/:symbol" element={<StockDetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  )
+}
