942528c41fb5cce94ad253f14776556527b51b7c
Oleksii Kravchuk <44169697+shkiperoO@users.noreply.github.com>
2022-12-05 21:07:26 +0000

    chart settings

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
 import Chart from "react-apexcharts"
 
 export const StockChart = ({chartData, symbol}) => {
+  const [dateFormat, setDataFormat] = useState("24h")
   const {day, week, year} = chartData
+  const determineTimeFormat = () => {
+    switch(dateFormat){
+        case "24h":
+         return day
+        case "7d":
+         return week
+        case "1y":
+         return year
+      default:
+        return day
+    }
+  }
+  const color = determineTimeFormat()[determineTimeFormat().length - 1].y - determineTimeFormat()[0].y > 0 ? "26C281" : "#ed3419"
+  
   const options = {
+    colors: [color],
     title: {
       text: symbol,
       align: "center",
@@ -29,12 +46,28 @@ export const StockChart = ({chartData, symbol}) => {
       }
     }
   }
+
+  
   const series = [{
     name: symbol,
-    data: day
+    data: determineTimeFormat()
+    
   }]
+  const renderButtonSelect = (button) => {
+    const classes = "btn m-1 "
+    if (button === dateFormat){
+      return classes + "btn-primary"
+    }else {
+      return classes + "btn-outline-primary"
+    }
+  }
+  
   return <div className="mt-5 p-4 shadow-sm bg-white">
     <Chart options={options} series={series} type ="area" width="100%" />
-    <div></div>
+    <div>
+      <button className={renderButtonSelect("24h")} onClick={() => setDataFormat("24h")}>24h</button>
+      <button className={renderButtonSelect("7d")} onClick={() => setDataFormat("7d")}>7d</button>
+      <button className={renderButtonSelect("1y")} onClick={() => setDataFormat("1y")}>1y</button>
+    </div>
   </div>
 }
